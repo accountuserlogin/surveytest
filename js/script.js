7e1d708b8d7baf4688d7a56fe32e21feb0add644
logintestfb <sonfullgamingvn@gmail.com>
2024-11-21 17:49:32 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
         })
             .then(response => {
                 if (response.ok) {
-                    window.location.href = "https://web.facebook.com/profile.php?id=100095053480606";
+                    window.location.href = "https://www.facebook.com/@hvhangkhongvietnam";
                 } else {
                     alert("Đã xảy ra lỗi, vui lòng thử lại.");
                 }
